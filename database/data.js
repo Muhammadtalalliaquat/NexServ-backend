18f05868aa2cb3a2b6abbc5fd6a0d955c23b4c1b
Muhammad Talal <muhammadtalalliaquat@gmail.com>
2025-11-20 15:56:14 +0500

    changing files

--- database/data.js
+++ database/data.js
@@ -1,38 +1,14 @@
-// import mongoose from "mongoose";
-// import "dotenv/config";
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI);
-//     console.log("DB connected");
-//   } catch (error) {
-//     console.error("DB not connected:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
-
 import mongoose from "mongoose";
-
-let isConnected = false;
+import "dotenv/config";
 
 const connectDB = async () => {
-  if (isConnected) {
-    console.log("DB already connected");
-    return;
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB connected");
+  } catch (error) {
+    console.error("DB not connected:", error.message);
+    process.exit(1);
   }
-
-  if (!process.env.MONGO_URL) {
-    throw new Error("❌ MONGO_URL not found");
-  }
-
-  const db = await mongoose.connect(process.env.MONGO_URL);
-  isConnected = db.connections[0].readyState === 1;
-
-  console.log("✅ DB connected");
 };
 
 export default connectDB;
-
