fe505ef275a65c4d0d29721608bd8a6925b55525
Muhammad Talal <muhammadtalalliaquat@gmail.com>
2025-11-20 16:31:34 +0500

    remove vercel file and create api folder setup vercel config

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import serviceRoutes from "./routes/serviceRoute.js";
+import userServiceRoutes from "./routes/userServiceRoute.js";
+import blogsRoutes from "./routes/blogRoute.js";
+import contactRoutes from "./routes/contactRoute.js";
+import morgan from "morgan";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.use("/user", authRoutes);
+app.use("/blogs", blogsRoutes);
+app.use("/service", serviceRoutes);
+app.use("/contact", contactRoutes);
+app.use("/user-service", userServiceRoutes);
+
+export default app;
--- index.js
+++ index.js
@@ -1,40 +1,13 @@
-import express from "express";
-import authRoutes from "./routes/auth.js";
-import serviceRoutes from "./routes/serviceRoute.js";
-import userServiceRoutes from "./routes/userServiceRoute.js";
-import blogsRoutes from "./routes/blogRoute.js";
-import contactRoutes from "./routes/contactRoute.js";
-import morgan from "morgan";
+import app from "./app.js";
 import connectDB from "./database/data.js";
-import cors from "cors";
-import "dotenv/config";
 
-const app = express();
-const PORT = process.env.PORT;
-
-app.use(express.json());
-app.use(cors());
-app.use(morgan("dev"));
-
+const PORT = process.env.PORT || 3000;
 
 connectDB()
   .then(() => {
-    app.get("/", (req, res) => {
-      res.send("Server is running and DB is connected");
-    });
-
-    app.use("/user", authRoutes);
-    app.use("/blogs", blogsRoutes);
-    app.use("/service", serviceRoutes);
-    app.use("/contact", contactRoutes);
-    app.use("/user-service", userServiceRoutes);
-
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    // const api = functions.https.onRequest(app);
   })
   .catch((err) => {
     console.error("DB not connected Server is not running:", err.message);
-    process.exit(1); // Exit the process if DB connection fails
+    process.exit(1);
   });
-
-
