b0c3c40a0fb6b6810c00c9181a0cdccff881c0b1
Muhammad Talal <muhammadtalalliaquat@gmail.com>
2025-12-26 16:36:21 +0500

    update changing

--- index.js
+++ index.js
@@ -37,3 +37,40 @@
 //   app.use("/user-service", userServiceRoutes);
 
 //   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import serviceRoutes from "./routes/serviceRoute.js";
+import userServiceRoutes from "./routes/userServiceRoute.js";
+import blogsRoutes from "./routes/blogRoute.js";
+import contactRoutes from "./routes/contactRoute.js";
+import reviewRoutes from "./routes/reviewRoute.js";
+import connectDB from "./database/data.js";
+import morgan from "morgan";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors("*"));
+app.use(morgan("dev"));
+
+connectDB()
+ 
+
+app.get("/", (req, res) => {
+  res.send("Server is running and DB is connected");
+});
+
+app.use("/user", authRoutes);
+app.use("/blogs", blogsRoutes);
+app.use("/service", serviceRoutes);
+app.use("/contact-us", contactRoutes);
+app.use("/user-review", reviewRoutes);
+app.use("/user-service", userServiceRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
