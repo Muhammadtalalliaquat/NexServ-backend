24d512ea1d8cbc715907317801d3b7ec5cd5ffac
Muhammad Talal <muhammadtalalliaquat@gmail.com>
2025-12-30 14:35:26 +0500

    update changing file code

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./database/data.js";
+import userServiceRoutes from "./routes/userServiceRoute.js";
+import serviceRoutes from "./routes/serviceRoute.js";
+import contactRoutes from "./routes/contactRoute.js";
+import blogsRoutes from "./routes/blogRoute.js";
+import reviewRoutes from "./routes/reviewRoute.js";
+import authRoutes from "./routes/auth.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors("*"));
+app.use(morgan("dev"));
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Server is running and DB is connected");
+});
+
+app.use("/user", authRoutes);
+app.use("/blogs", blogsRoutes);
+app.use("/service", serviceRoutes);
+app.use("/contact-us", contactRoutes);
+app.use("/user-review", reviewRoutes);
+app.use("/user-service", userServiceRoutes);
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
+export default app;
